ae0f1a4b23999fc050f0da70f376f36093e201ce
Sharad Jain <shjain@expedia.com>
2017-09-18 16:40:17 +0530

    email reports using results

---
+++ bin/email-generate.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+const program = require('commander');
+const package = require("../package.json");
+var swig = require('swig');
+var template = swig.compileFile(__dirname + '/../emailTemplate.html');
+fs = require('fs');
+
+program
+    .version(package.version)
+    .option('-p, --project [name]', 'Name of the project')
+    .option('-i, --input [resultsdir]', 'Results directory', './resultsJSON')
+    .option('-o, --output [type]', 'Output html path [dir]', process.cwd() + "/emailableReport.html").
+    parse(process.argv);
+if (program.input && program.project) {
+    require("../lib/index")(process.cwd() + program.input, function (result) {
+        fs.writeFile(program.output, template({"result": result}), function (err) {
+            if (err) {
+                return console.log(err);
+                process.exit(1);
+            } else {
+                process.exit(0);
+            }
+        });
+    });
+} else {
+    throw new Error("Please provide mandatory params project and input.")
+}
